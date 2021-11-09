15c600d1d6b79d5513b2451eddcea5d3e0ce5056
Alan Perez <diegoalanperezz@gmail.com>
2021-11-09 13:26:13 -0300

    Fix download frontend, fix header

--- routes/words.routes.js
+++ routes/words.routes.js
@@ -10,64 +10,67 @@ const route = Router();
 const upload = multer({ dest: "uploads/" });
 
 route
-  .get("/", async (req, res) => {
-    // gets the words in the DB and transforms them into a CSV file wich is served
-    const filePath = path.join(process.cwd(), "uploads/words.csv");
+    .get("/", async (req, res) => {
+        // gets the words in the DB and transforms them into a CSV file wich is served
+        const filePath = path.join(process.cwd(), "uploads/words.csv");
 
-    const controller = new WordController();
-    const words = await controller.getWords();
+        const controller = new WordController();
+        const words = await controller.getWords();
 
-    const csvWriter = createObjectCsvWriter({
-      path: filePath,
-      header: [
-        { id: "_id", title: "_id" },
-        { id: "originalWord", title: "Palabra" },
-        { id: "text", title: "Texto" },
-        { id: "textInfo", title: "Informacion de texto" },
-        { id: "source", title: "Fuente" },
-        { id: "active", title: "Activo" },
-      ],
-    });
-
-    csvWriter.writeRecords(words).then(() => {
-      res.download(filePath);
-    });
-  })
-  .post("/", upload.single("csvFile"), (req, res) => {
-    const {
-      filename: fileName,
-      destination: uploadFolder,
-      originalname: fileNameNew,
-    } = req.file;
-
-    const oldPath = uploadFolder + fileName;
-    const newPath = uploadFolder + fileNameNew;
-
-    fs.rename(oldPath, newPath, () => {});
-
-    if (fileNameNew.split(".").pop() !== "csv") {
-      fs.unlink(oldPath, () => {});
-
-      res.send("El archivo debe ser en formato CSV");
-    } else {
-      const words = [];
-
-      fs.createReadStream(newPath)
-        .pipe(csvParser({ headers: true, separator: "," }))
-        .on("data", (data) => {
-          words.push(data);
-        })
-        .on("end", () => {
-          res.json(words);
+        const csvWriter = createObjectCsvWriter({
+            path: filePath,
+            header: [
+                { id: "_id", title: "_id", },
+                { id: "originalWord", title: "originalWord", },
+                { id: "text", title: "text", },
+                { id: "textInfo", title: "textInfo", },
+                { id: "source", title: "source", },
+                { id: "active", title: "active", },
+            ],
         });
-    }
-  })
-  .get("/active", async (req, res) => {
-    // gets the active words in the DB
-    const controller = new WordController();
-    const words = await controller.getActiveWords();
 
-    res.json(words);
-  });
+        csvWriter.writeRecords(words).then(() => {
+            res.setHeader("Content-Type", "text/csv");
+            res.download(filePath);
+        });
+    })
+    .post("/", upload.single("csvFile"), (req, res) => {
+        const {
+            filename: fileName,
+            destination: uploadFolder,
+            originalname: fileNameNew,
+        } = req.file;
+
+        const oldPath = uploadFolder + fileName;
+        const newPath = uploadFolder + fileNameNew;
+
+        fs.rename(oldPath, newPath, () => { });
+
+        if (fileNameNew.split(".").pop() !== "csv") {
+            fs.unlink(oldPath, () => { });
+
+            res.send("El archivo debe ser en formato CSV");
+        } else {
+            const words = [];
+
+            fs.createReadStream(newPath)
+                .pipe(csvParser({ headers: true, separator: "," }))
+                .on("data", (data) => {
+                    words.push(data);
+                })
+                .on("end", () => {
+                    // header excel csv
+                    res.setHeader("Content-Type", "text/csv");
+                    res.json(words);
+                });
+        }
+    })
+    .get("/active", async (req, res) => {
+        // gets the active words in the DB
+        const controller = new WordController();
+        const words = await controller.getActiveWords();
+
+        res.json(words);
+    });
 
 export default route;
